29f61457c54e496b5a58b172061b12c4600de61e
giovani <115195600+giovanep4mg@users.noreply.github.com>
2023-02-06 13:16:37 -0300

    ATUALIZANDO,CASA

--- index_soma.js
+++ index_soma.js
@@ -6,6 +6,8 @@ const num1s = Math.ceil(Math.random() * 10)
 const num2s = Math.ceil(Math.random() * 10)
 console.log("numero1 " + num1s)
 console.log("numero2 " + num2s)
+
+
 //pega o que está no id "questaoSoma" e salva na variável "questaoSomaE1".
 const questaoSomaE1 = document.getElementById("questaoSoma");
 console.log("pergunta" + questaoSomaE1.innerText)
@@ -19,13 +21,13 @@ const formSomaE1 = document.getElementById("formSoma");
 console.log("titulo, ponto, pergunta, campo de texto :" + formSomaE1.innerText)
 
 //pega o que está no id "pontoS" e salva na variável "pontoSE1".
-const pontoSE1 = document.getElementById("pontoS")
-console.log("texto sobre pontos e quanto tem  " + pontoSE1.innerText)
+const pontoSomaE1 = document.getElementById("pontoS")
+console.log("texto sobre pontos e quanto tem  " + pontoSomaE1.innerText)
 
 
 //
 let pontoS = JSON.parse(localStorage.getItem("pontoS"));
-console.log(pontoS.innerText)
+console.log("pontos:  " + pontoS)
 
 
 /*
@@ -39,48 +41,58 @@ if (!pontoS) {
 }
 console.log("SE PONTOS:  " + pontoS)
 
+
 //pega o valor na var "pontoE1" e exibi na tela
-pontoSE1.innerText = "ponto :" + pontoS;
-console.log("para exibir quantos pontos tem  " + pontoSE1)
+pontoSomaE1.innerText = "ponto :" + pontoS;
+console.log("para exibir quantos pontos tem  " + pontoSomaE1.innerText)
+
 
 //pega os valores da var "questaoE1", e exibi na tela, com algumas modificações nos números.
 questaoSomaE1.innerText = "Quanto é " + num1s + " + " + num2s + " ? "
-console.log("exibi a pergunta com numeros aleatorios:  " + questaoSomaE1)
+console.log("exibi a pergunta com numeros aleatorios:  " + questaoSomaE1.innerText)
+
+
 
 //pega os números e multiplica eles, depois salva na var "correctAns"(resposta_correta)
 const correctSomaAns = num1s + num2s;
-console.log("soma os numeros, para corrigir  " + correctSomaAns)
+console.log("soma os numeros, para corrigir  " + correctSomaAns.innerText)
+
+
+
 
 //adiciona um ouvinte, no botão "submit"
 formSomaE1.addEventListener("submit", () => {
 
     //pega o valor adicionado no "input",e salva na var "userAns"(resposta_usuario)
     const userAnsSoma = +inputSomaE1.value;
+    console.log("valor adicionado no input" + userAnsSoma.innerText)
 
     //se (resposta_usuario é igual a resposta_correta)
     if (userAnsSoma == correctSomaAns) {
 
         //se for igual, adiciona +1 na var "ponto"
         pontoS++;
+        console.log("pontos a mais no pontos:   " + pontoS++)
         //executa a função "updateLocalStorage()"
-        updateLocalStorageSoma()
+        updateLocalStorage()
         //senão
     } else {
 
         //se não for igual, retira -1, na var "ponto"
         pontoS--;
+        console.log("pontos a menos no pontos:   " + pontoS--)
         //executa a função "updateLocalStorage()"
-        updateLocalStorageSoma()
+        updateLocalStorage()
     }
 
 })
-console.log(" modifica a form, faz a comparaçao do numeros,se acertou ou nao, e salva no ponto  :" + formSomaE1.innerText)
+console.log(" modifica a form, faz a comparaçao do numeros,se acertou ou nao, e salva no ponto  :" + formSomaE1)
 
 
-function updateLocalStorageSoma() {
-    localStorage.setItem("ponto", JSON.stringify(+pontoS))
+function updateLocalStorage() {
+    localStorage.setItem("pontoS", JSON.stringify(+pontoS))
 }
-console.log(updateLocalStorageSoma())
 
 
-console.log("update local storage soma  :" + updateLocalStorageSoma())
+
+console.log("update local storage soma  :" + updateLocalStorage())
